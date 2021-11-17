22a006b8ef125eb0c6bdb898665240a7eb7bd4d6
happy2h <xienick520lsy@gmail.com>
2021-11-17 15:53:04 +0800

    Update MFAILC.m
    更新代码

--- MFAILC.m
+++ MFAILC.m
@@ -8,11 +8,11 @@ for k = 1:1:500
 end
 
 % 参数设置
-epsilon = 0.01;
-eta = 1;
-rho = 0.2;
-lamda = 1;
-mu = 2;
+epsilon = 0.01; % 拟伪偏导重置阈值
+eta = 1; % 拟伪偏导更新增益
+rho = 0.2; % 控制律增益
+lamda = 1; % 控制律步长
+mu = 2; % 拟伪偏导更新步长
 
 % 控制过程
 i_n = 60;  %迭代次数
@@ -20,19 +20,19 @@ y(1:i_n,1:500) = 0;
 for i = 1:1:i_n
 	for k = 1:1:500
 		if k == 1
-			phi(i,k) = 0.4;
+			phi(i,k) = 0.4; % 初始拟伪偏导
 		elseif k == 2
 			phi(i,k) = phi(i,k-1) + (eta*(ub(i,k-1) - 0)/(mu + norm(ub(i,k-1) - 0)^2))*(y(i,k) - 0 - phi(i,k-1)*(ub(i,k-1) - 0));
 		else 
 			phi(i,k) = phi(i,k-1) + (eta*(ub(i,k-1) - ub(i,k-2))/(mu + norm(ub(i,k-1) - ub(i,k-2))^2))*(y(i,k) - y(i,k-1) - phi(i,k-1)*(ub(i,k-1) - ub(i,k-2)));
 		end
 		if i == 1
-			uf(i,k) = 0;
+			uf(i,k) = 0; % 初始控制信号
 		else 
 			uf(i,k) = uf(i-1,k) + 0.4*e(i-1,k+1);
 		end
 		if k == 1
-			ub(i,k) = 0;
+			ub(i,k) = 0; % 初始控制信号
 		else 
 			ub(i,k) = ub(i,k-1) + (rho*phi(i,k)/(lamda + norm(phi(i,k))^2))*(yd(k+1) - y(i,k));
 		end
@@ -40,9 +40,9 @@ for i = 1:1:i_n
 			phi(i,k) = phi(i,1);
 		end
 		u(i,k) = uf(i,k) + ub(i,k);
-		%系统函数
-		if k <250 
-			y(i,k+1) = y(i,k)*u(i,k)/(1 + norm(y(i,k))^2) + (u(i,k) + 0.1*round(k/500)*sin(y(i,k)))^3;
+		% 系统函数
+		if k < 250 
+			y(i,k+1) = y(i,k)*u(i,k)/(1 + norm(y(i,k))^2) + (u(i,k) + (1+*round(k/500))*sin(y(i,k)))^3;
 		else
 			y(i,k+1) = y(i,k)*u(i,k)^3/(1 + norm(y(i,k))^2) + u(i,k)^3;
 		end
